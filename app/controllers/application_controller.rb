ace28e044ec71e61b2ae2fc0476f19291815a467
Marcos Scorzoni <mlessa@gmail.com>
2018-04-11 12:12:59 -0300

    Functioning Lat and Long for User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :address, :phone_number, :blood_type, :available_monday, :available_tuesday, :available_wednesday, :available_thursday, :available_friday, :available_saturday, :available_sunday  ])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :address, :phone_number, :blood_type, :available_monday, :available_tuesday, :available_wednesday, :available_thursday, :available_friday, :available_saturday, :available_sunday, :latitude, :longitude  ])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys:[:first_name, :last_name, :address, :phone_number, :blood_type, :available_monday, :available_tuesday, :available_wednesday, :available_thursday, :available_friday, :available_saturday, :available_sunday  ])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:first_name, :last_name, :address, :phone_number, :blood_type, :available_monday, :available_tuesday, :available_wednesday, :available_thursday, :available_friday, :available_saturday, :available_sunday, :latitude, :longitude ])
   end
 end
