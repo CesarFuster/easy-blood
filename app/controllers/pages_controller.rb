2a3296341367662bd90e5c05c40247f05c2e62e2
Marcos Scorzoni <mlessa@gmail.com>
2018-04-11 15:52:49 -0300

    Yarn Pack Gmaps installed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :index]
 
   def home
+    @users = User.where.not(latitude: nil, longitude: nil)
+
+    @markers = @users.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
   end
+
 end
