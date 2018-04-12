ff1e0f51a8e6e9dee74eb79f3f2a054c9b9c2416
cesarfuster <cfuster@uol.com.br>
2018-04-12 07:36:49 -0300

    Layout index campaigns

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@ User.destroy_all
 puts "Seeding 4 institutions..."
 
 institution1 = Institution.create(name: 'Einstein', address: 'Av. Paulista, 900', email_resp: 'einstein@email.com.br', name_resp: 'João', phone_number_resp: '9999-9999')
-institution2 = Institution.create(name: 'Einstein 2', address: 'Av. Paulista, 900', email_resp: 'einstein@email.com.br', name_resp: 'João 2', phone_number_resp: '9999-9999')
-institution3 = Institution.create(name: 'Einstein 3', address: 'Av. Paulista, 900', email_resp: 'einstein@email.com.br', name_resp: 'João 3', phone_number_resp: '9999-9999')
-institution4 = Institution.create(name: 'Einstein 4', address: 'Av. Paulista, 900', email_resp: 'einstein@email.com.br', name_resp: 'João 4', phone_number_resp: '9999-9999')
+institution2 = Institution.create(name: 'São Luiz', address: 'Av. Paulista, 900', email_resp: 'einstein@email.com.br', name_resp: 'João 2', phone_number_resp: '9999-9999')
+institution3 = Institution.create(name: 'Santa Joana', address: 'Av. Paulista, 900', email_resp: 'einstein@email.com.br', name_resp: 'João 3', phone_number_resp: '9999-9999')
+institution4 = Institution.create(name: 'Prefeitura SP', address: 'Av. Paulista, 900', email_resp: 'einstein@email.com.br', name_resp: 'João 4', phone_number_resp: '9999-9999')
 
-puts "Seeding 5 campaigns..."
+puts "Seeding 25 campaigns..."
 
 campaign1 = Campaign.create(start_date: (Date.today + 5), end_date: (Date.today + 10), location: 'Beco do Batman', institution_id: 1);
 campaign2 = Campaign.create(start_date: Date.today, end_date: (Date.today + 20), location: 'Av Paulista', institution_id: 1);
@@ -17,6 +17,25 @@ campaign3 = Campaign.create(start_date: (Date.today + 1), end_date: (Date.today 
 campaign4 = Campaign.create(start_date: (Date.today + 10), end_date: (Date.today + 15), location: 'Google', institution_id: 3);
 campaign5 = Campaign.create(start_date: Date.today, end_date: (Date.today + 5), location: 'Beco do Batman', institution_id: 4);
 campaign6 = Campaign.create(start_date: Date.today, end_date: (Date.today + 8), location: 'Beco', institution_id: 2);
+campaign7 = Campaign.create(start_date: (Date.today + 5), end_date: (Date.today + 10), location: 'Beco do Batman', institution_id: 1);
+campaign8 = Campaign.create(start_date: Date.today, end_date: (Date.today + 20), location: 'Av Paulista', institution_id: 1);
+campaign9 = Campaign.create(start_date: (Date.today + 1), end_date: (Date.today + 5), location: 'Lewgon', institution_id: 2);
+campaign10 = Campaign.create(start_date: (Date.today + 10), end_date: (Date.today + 15), location: 'Google', institution_id: 3);
+campaign11 = Campaign.create(start_date: Date.today, end_date: (Date.today + 5), location: 'Beco do Batman', institution_id: 4);
+campaign12 = Campaign.create(start_date: Date.today, end_date: (Date.today + 8), location: 'Beco', institution_id: 2);
+campaign13 = Campaign.create(start_date: (Date.today + 5), end_date: (Date.today + 10), location: 'Beco do Batman', institution_id: 1);
+campaign14 = Campaign.create(start_date: Date.today, end_date: (Date.today + 20), location: 'Av Paulista', institution_id: 1);
+campaign15 = Campaign.create(start_date: (Date.today + 1), end_date: (Date.today + 5), location: 'Lewgon', institution_id: 2);
+campaign16 = Campaign.create(start_date: (Date.today + 10), end_date: (Date.today + 15), location: 'Google', institution_id: 3);
+campaign17 = Campaign.create(start_date: Date.today, end_date: (Date.today + 5), location: 'Beco do Batman', institution_id: 4);
+campaign18 = Campaign.create(start_date: Date.today, end_date: (Date.today + 8), location: 'Beco', institution_id: 2);
+campaign19 = Campaign.create(start_date: (Date.today + 5), end_date: (Date.today + 10), location: 'Beco do Batman', institution_id: 1);
+campaign20 = Campaign.create(start_date: Date.today, end_date: (Date.today + 20), location: 'Av Paulista', institution_id: 1);
+campaign21 = Campaign.create(start_date: (Date.today + 1), end_date: (Date.today + 5), location: 'Lewgon', institution_id: 2);
+campaign22 = Campaign.create(start_date: (Date.today + 10), end_date: (Date.today + 15), location: 'Google', institution_id: 3);
+campaign23 = Campaign.create(start_date: Date.today, end_date: (Date.today + 5), location: 'Beco do Batman', institution_id: 4);
+campaign24 = Campaign.create(start_date: Date.today, end_date: (Date.today + 8), location: 'Beco', institution_id: 2);
+campaign25 = Campaign.create(start_date: Date.today, end_date: (Date.today + 8), location: 'Beco', institution_id: 2);
 
 
 puts "Seeding 5 users..."
