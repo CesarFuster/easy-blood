68761aaf8c6d452a17c6f75ce84c1bb063c23eef
cesarfuster <cfuster@uol.com.br>
2018-04-13 12:02:10 -0300

    Validations included

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@ class User < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :blood_type, presence: true
+
   private
 
   def send_welcome_email
