f0f4cb055e5ad68509f4feb41c3f725c612e3db8
cesarfuster <cfuster@uol.com.br>
2018-04-13 11:39:11 -0300

    Show cards home done by date

--- app/services/campaign_creator.rb
+++ app/services/campaign_creator.rb
@@ -1,13 +1,13 @@
 class CampaignCreator
   def self.run
     puts "Campaign created*********************************"
-    Campaign.create(institution: Institution.all.sample, cpoint: self.generate.first, location: self.generate.first.address) if self.generate.length > 0
+    Campaign.create(institution: Institution.all.sample, start_date: Date.today, end_date: (Date.today + 1), cpoint: self.generate.first, location: self.generate.first.address) if self.generate.length > 0
   end
 
   def self.generate
     array = []
     Cpoint.all.each do |cpoint|
-      array << cpoint if User.near(cpoint.address, 5).length > 4
+      array << cpoint if User.near(cpoint.address, 5).length > 1
     end
     return array
   end
