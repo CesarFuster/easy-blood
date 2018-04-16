b59ec2e19d197f77faf599e05e11c98d4ba03d95
andre miotto <miotto082@gmail.com>
2018-04-16 16:47:04 -0300

    atalho para campaign-id e swal confirmando presenca

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,13 @@
 import "bootstrap";
 import swal from 'sweetalert';
+
+
+document.getElementById("confirmMe").addEventListener("click", message);
+
+
+function message() {
+    swal("Obrigado pela confirmação!", "Seu gesto poderá salvar uma vida! Nos vemos em breve");
+    document.getElementById("confirmMe").innerHTML = 'Presença confirmada';
+    document.getElementById("confirmMe").removeEventListener('click', message);
+}
+
