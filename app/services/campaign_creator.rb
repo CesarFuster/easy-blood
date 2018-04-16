3abc873c53a9b8cb7e549cefc0c908a9815d74a1
andre miotto <miotto082@gmail.com>
2018-04-16 14:17:06 -0300

    botao envianbdo para campaign id

--- app/services/campaign_creator.rb
+++ app/services/campaign_creator.rb
@@ -2,12 +2,13 @@ class CampaignCreator
   #cria campanha depois se o check do contador de pessoas ao redor do cpoint for true
   def self.run(new_hash)
       puts "Campaign created  *********************************"
-      Campaign.create(
-        institution: Institution.all.sample, 
+      campaign = Campaign.create(
+        institution: Institution.all.sample,
         cpoint: new_hash[:cpoint].first,
         location: new_hash[:cpoint].first.address,
         start_date: (Date.today),
         end_date: (Date.today + 1)
       )
+      campaign
   end
 end
