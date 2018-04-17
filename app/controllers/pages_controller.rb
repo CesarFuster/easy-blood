7944218460ab9810a558cbf0027b280c2bfca242
Thiago <thiago@icity.com.br>
2018-04-17 14:47:46 -0300

    page about_us, with routes fixing, and images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:home, :index, :about_us]
 
   def home
     @source = Rails.application.routes.recognize_path(request.referrer) unless request.referrer.nil?
@@ -12,5 +12,9 @@ class PagesController < ApplicationController
         lng: user.longitude
       }
     end
+  end
+
+  def about_us
+    @team = {thiago: {name: "Thiago Scatigno", email: "thiago@icity.com.br", phrase: "Equipe nota 10"}, marcos: {name: "Marcos Lessa", email: "mlessa@gmail.com", phrase: "O Brazil eh uma merda"}, andre: {name: "Andre Miotto", email: "miotto082@gmail.com", phrase: "Whaaaaaaattttt"}, cesar: {name: "Cesar Fuster", email: "cfuster@uol.com.br", phrase: "Uai o que? Sua bicha"}}
   end
 end
